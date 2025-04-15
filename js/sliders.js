c09cfa04ba61dbd8163a4ad5bd4bf6f5561f8317
Nikita Zlobin <nikjke@mail.ru>
2025-04-15 19:09:24 +0300

    feat: add blocks to home page

--- js/sliders.js
+++ js/sliders.js
@@ -1,3 +1,26 @@
+if (jQuery(".main-screen__slider").length) {
+  jQuery(".main-screen__slider")
+    .not(".slick-initialized")
+    .slick({
+      // slidesToShow: 1,
+      dots: true,
+      appendArrows: jQuery(".main-screen__controls"),
+      appendDots: jQuery(".main-screen__controls"),
+      prevArrow:
+        '<button class="slick-prev">\n' +
+        '  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M15 18L9 12L15 6" stroke="#949494" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />\n' +
+        '  </svg>\n' +
+        '</button>',
+      nextArrow:
+        '<button class="slick-next">\n' +
+        '  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">\n' +
+        '    <path d="M9 18L15 12L9 6" stroke="#B20003" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />\n' +
+        '  </svg>\n' +
+        '</button>',
+    });
+}
+
 if (jQuery(".clients__slider").length) {
   jQuery(".clients__slider")
     .not(".slick-initialized")
